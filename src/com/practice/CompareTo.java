b58b177c73767d1744b768a3b2c96f3df6454c79
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-02-07 14:01:34 +0530

    Added : Two Line Compare To Method

---
+++ src/com/practice/CompareTo.java
@@ -0,0 +1,13 @@
+package com.practice;
+import java.lang.String;
+public class CompareTo {
+    public static void main(String[] args) {
+
+
+        String string1 = "TANUJA";
+        String string2 = "tanuja";
+        System.out.println(string1.compareTo(string2));
+        System.out.println(string2.compareTo(string1));
+
+    }
+}
